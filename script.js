b6c8d06072360a4b1cd0f0aa87cde9b67b8d2a3e
Felipe Dutra <127852691+Bullsyeswild@users.noreply.github.com>
2023-11-22 21:29:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const menu = document.querySelector('.btn-menu');
 const menuMobile = document.querySelector('.menu-mobile');
 const linksMobile = document.querySelectorAll('.link-mobile');
+const gifsInfo = [
+    { id: 'meuGif1', intervalo: 10000 }, 
+    { id: 'meuGif2', intervalo: 30000 },
+    { id: 'meuGif3', intervalo: 40000 }, 
+    { id: 'meuGif4', intervalo: 25000 },
+    { id: 'meuGif5', intervalo: 48000 } 
+];
 
 menu.addEventListener('click', () => {
     menu.classList.toggle('ativo');
@@ -20,3 +27,17 @@ menu.addEventListener('click', () => {
         document.body.style.overflow = '';
     }
 });
+
+function restartGifById(id) {
+    const gif = document.getElementById(id);
+    gif.src = gif.src;
+}
+
+function restartGifs() {
+    gifsInfo.forEach(gif => {
+        setInterval(() => restartGifById(gif.id), gif.intervalo);
+    });
+}
+
+restartGifs();
+
