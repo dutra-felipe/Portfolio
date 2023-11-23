49f6abe4256436dcdd044db438f51afa3a1ebf8b
Felipe Dutra <127852691+Bullsyeswild@users.noreply.github.com>
2023-11-22 21:42:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const menuMobile = document.querySelector('.menu-mobile');
 const linksMobile = document.querySelectorAll('.link-mobile');
 const gifsInfo = [
     { id: 'meuGif1', intervalo: 10000 }, 
-    { id: 'meuGif2', intervalo: 30000 },
-    { id: 'meuGif3', intervalo: 40000 }, 
-    { id: 'meuGif4', intervalo: 25000 },
-    { id: 'meuGif5', intervalo: 48000 } 
+    { id: 'meuGif2', intervalo: 10000 },
+    { id: 'meuGif3', intervalo: 10000 }, 
+    { id: 'meuGif4', intervalo: 11000 },
+    { id: 'meuGif5', intervalo: 12000 }
 ];
 
 menu.addEventListener('click', () => {
